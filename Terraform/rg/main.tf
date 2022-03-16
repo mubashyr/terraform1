6d925d9c50f36fdb41f544328a1c7096522ac08f
mubashyr <81147854+mubashyr@users.noreply.github.com>
2022-03-16 17:32:57 +0530

    Delete Terraform directory

--- Terraform/rg/main.tf
+++
@@ -1,4 +0,0 @@
-resource "azurerm_resource_group" "rg" {
-  name     = "terraform-rg"
-  location = "eastus2"
-}
--- Terraform/vm/httpd.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-sudo apt update
-sudo apt-get install nginx -y
-sudo systemctl enable nginx
-sudo systemctl start nginx
